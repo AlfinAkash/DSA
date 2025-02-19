4705de1b4f8e80b7635b1ee3308a67c0ce7bbb2b
Akash A <113112052+AlfinAkash@users.noreply.github.com>
2025-02-20 03:09:16 +0530

    Update MColoringProblem.java

--- backtracking/MColoringProblem.java
+++ backtracking/MColoringProblem.java
@@ -5,7 +5,7 @@ public class MColoringProblem {
     // Number of vertices in the graph
     static int V = 4;
 
-
+//print 
     static  void printSolution(int[] color){
         System.out.println("Solution Exists:" + "Following are the assigned colors ");
         for(int i = 0 ; i < V; i++){
