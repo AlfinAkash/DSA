fed9ee075dead7013cd1a026da1bde4b006019c2
Akash A <113112052+AlfinAkash@users.noreply.github.com>
2025-03-20 12:07:00 +0530

    Update BM_01.java

--- bitManipulation/BM_01.java
+++ bitManipulation/BM_01.java
@@ -6,7 +6,7 @@ package bitManipulation;
  */
 public class BM_01 {
 
-	// Function to get no of set bits in binary representation of positive integer n 
+// Function to get no of set bits in binary representation of positive integer n 
 	static int countSetBits(int n) {
 		int count = 0;
 		while(n > 0) {
